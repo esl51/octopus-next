66cad6c264e3f960c974438f23f3c5284e675696
Maxim Zhgulev <max@8max.ru>
2022-11-09 19:20:12 +0300

    need to fetch user on locale change

--- resources/js/middleware/locale.ts
+++ resources/js/middleware/locale.ts
@@ -1,4 +1,4 @@
-import { setLanguage } from '@/plugins/i18n'
+import i18n, { setLanguage } from '@/plugins/i18n'
 import { useAuthStore } from '@/stores/auth'
 import { useLangStore } from '@/stores/lang'
 import { MiddlewareInterface } from '@/types'
@@ -7,8 +7,10 @@ const langStore = useLangStore()
 const authStore = useAuthStore()
 
 export default ({ next }: MiddlewareInterface) => {
+  const needToFetchUser =
+    authStore.check && langStore.locale !== i18n.global.locale.value
   setLanguage(langStore.locale)
-  if (authStore.check) {
+  if (needToFetchUser) {
     authStore.fetchUser()
   }
 
