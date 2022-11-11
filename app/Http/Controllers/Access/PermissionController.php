aa9160ea25c8332932950212982e70ff0ac474b6
Maxim Zhgulev <max@8max.ru>
2022-11-11 10:16:26 +0300

    fix permission controller validation

--- app/Http/Controllers/Access/PermissionController.php
+++ app/Http/Controllers/Access/PermissionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Access;
 
-use App\Http\Controllers\ItemController;
-use App\Http\Resources\Access\PermissionResource;
 use App\Models\Permission;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Controllers\ItemController;
+use App\Http\Resources\Access\PermissionResource;
 
 class PermissionController extends ItemController
 {
@@ -22,7 +23,14 @@ class PermissionController extends ItemController
     public function getValidationRules(Request $request, $id = null)
     {
         return [
-            'name' => 'required|string|max:255',
+            'name' => [
+                'required',
+                'string',
+                'max:255',
+                Rule::unique('permissions')
+                    ->ignore($id)
+                    ->where(fn($query) => $query->where('guard_name', $request->input('guard_name'))),
+            ],
             'guard_name' => 'required|string|max:255',
         ];
     }
