3104bb9869c07a16a6b55d3454511ae9b9d79c3b
Maxim Zhgulev <max@8max.ru>
2022-11-24 11:31:23 +0300

    fix migrations

--- database/migrations/2022_11_18_164349_create_files_table.php
+++ database/migrations/2022_11_18_164349_create_files_table.php
@@ -47,6 +47,7 @@ return new class extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('file_translations');
         Schema::dropIfExists('files');
     }
 };
