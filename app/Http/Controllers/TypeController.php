616b366c45c2641137b9f2b11484c40bf6a96973
Maxim Zhgulev <max@8max.ru>
2023-08-31 20:26:02 +0300

    fix type controller

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -3,18 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\TypeResource;
-use App\Models\PropertyType;
 use Illuminate\Http\Request;
 
 class TypeController extends Controller
 {
-    /**
-     * Display a listing of property types.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function propertyTypes(Request $request)
-    {
-        return TypeResource::collection(PropertyType::cases());
-    }
+    //
 }
